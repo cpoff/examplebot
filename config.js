150cafb412a6496ec1936d5be14fc78c1b51a783
Curt Poff <curt.poff@gmail.com>
2016-02-28 11:28:09 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'blah',
+  consumer_key:         '0muIBkAeaM1vwaBeGBQUKfCGf',
   consumer_secret:      'blah',
   access_token:         'blah',
   access_token_secret:  'blah'
